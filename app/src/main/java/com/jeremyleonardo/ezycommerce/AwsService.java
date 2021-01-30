d5ae2904515dba957c4dc8f56f8c5313539195e7
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2021-01-30 20:16:35 +0700

    add getBookDetail from api

--- app/src/main/java/com/jeremyleonardo/ezycommerce/AwsService.java
+++ app/src/main/java/com/jeremyleonardo/ezycommerce/AwsService.java
@@ -2,12 +2,21 @@ package com.jeremyleonardo.ezycommerce;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface AwsService {
+
     @GET("book")
     Call<BooksResponse> getBooks(
             @Query(value="nim") String nim,
             @Query(value="nama") String nama
+    );
+
+    @GET("book/{id}")
+    Call<BookDetailResponse> getBookDetail(
+            @Path(value = "id", encoded = true) int id,
+            @Query(value = "nim") String nim,
+            @Query(value = "nama") String nama
     );
 }
